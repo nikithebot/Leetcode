4c2e0e69d4c9996c820f1e23ae9944731c016943
Kushmita Shah <121217232+nikithebot@users.noreply.github.com>
2026-08-10 20:38:06 +0530

    Time: 0 ms (100%), Space: 8.9 MB (50.82%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> ans;
-        long long val = 1;
+        vector<int> row = {1};
 
-        for(int k=0; k<=rowIndex; k++){
-            ans.push_back(val);
-            val = val*(rowIndex-k)/(k+1);
+        for(int i=1; i<=rowIndex; i++){
+            vector<int> temp(i+1, 1);
+            for(int j=1; j<i; j++){
+                temp[j] = row[j-1]+row[j];
+            }
+            row = temp;
         }
-
-        return ans;
+        return row;
     }
 };
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> getRow(int rowIndex) {
+//         vector<int> ans;
+//         long long val = 1;
+
+//         for(int k=0; k<=rowIndex; k++){
+//             ans.push_back(val);
+//             val = val*(rowIndex-k)/(k+1);
+//         }
+
+//         return ans;
+//     }
+// };
