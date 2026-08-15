7de23f1600f4e6253da22d4b4b4006ed68ef33d4
Kushmita Shah <121217232+nikithebot@users.noreply.github.com>
2026-08-15 13:33:09 +0530

    Time: 1 ms (97.52%), Space: 22.9 MB (55.64%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long hours(vector<int>&piles, int speed){
+        long long time=0;
+        for(auto bananas : piles){
+            time += (1LL*bananas+(long long)speed-1)/speed;
+        }
+        return time;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int low=1, high=*max_element(piles.begin(),piles.end());
+        int ans = high;
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            long long time = hours(piles,mid);
+
+            if(time<=h){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
