aafddcf1dc916f81b25cf098748525c2e2de1a1b
Kushmita Shah <121217232+nikithebot@users.noreply.github.com>
2026-09-21 20:00:38 +0530

    Time: 0 ms (100%), Space: 10.2 MB (6.11%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
+        stack<int> st;
         for(int i=0; i<s.size(); i++){
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{'){
-                st.push(s[i]);
+            char ch = s[i];
+
+            if(ch=='(' || ch=='{' || ch=='['){
+                st.push(ch);
             }else{
-                if(st.empty()) return false;
-                
-                char top = st.top();
-                if((top=='(' && s[i]!=')') || (top=='[' && s[i]!=']') || (top=='{' && s[i]!='}')){
+                if(st.empty()){
                     return false;
                 }else{
+                    int top = st.top();
                     st.pop();
+
+                    if((ch==')' && top!='(') || (ch=='}' && top!='{') || (ch==']' && top!='[')){
+                        return false;
+                    }
                 }
             }
         }
